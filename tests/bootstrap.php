96edb44dbf75ef0e4b7334c1295cf489c60d0908
USAMI Kenta <tadsan@zonu.me>
2019-01-24 15:58:28 +0900

    Add setup Whoops!

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bootstrap script for ZoPsr testing
+ *
+ * @copyright 2019 USAMI Kenta
+ * @license https://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
+ */
+
+error_reporting(E_ALL);
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PlainTextHandler);
+$whoops->register();
