4dd698cf9d2a84ff76eff06c61898209b5d2e323
USAMI Kenta <tadsan@zonu.me>
2019-01-24 17:22:57 +0900

    Add Phan and PHPStan

---
+++ tests/static_analysis.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function ZoPsr\Autoload0\add_directory;
+use ZoPsr\Autoload4\ClassLoader;
+
+
+add_directory(__DIR__ . '/Autoload0');
