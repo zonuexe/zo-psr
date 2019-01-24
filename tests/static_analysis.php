22a05e45cd731629b7ac5cfd7c79d531dcc6fef3
USAMI Kenta <tadsan@zonu.me>
2019-01-24 18:55:19 +0900

    Add class loader for static analysis

--- tests/static_analysis.php
+++ tests/static_analysis.php
@@ -5,5 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use function ZoPsr\Autoload0\add_directory;
 use ZoPsr\Autoload4\ClassLoader;
 
-
 add_directory(__DIR__ . '/Autoload0');
+
+$loader = new ClassLoader;
+$loader->register(false);
+$loader->add('Foo\\', __DIR__ . '/Autoload4/1');
+$loader->add('Hoge\\', __DIR__ . '/Autoload4/2');
