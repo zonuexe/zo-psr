0c0076bf16f89c10faa5f274fd731b4e8cb641c5
USAMI Kenta <tadsan@zonu.me>
2019-01-25 13:45:43 +0900

    名前空間名の _ は DIRECTORY_SEPARATOR に置換してはならない

--- src/Autoload0/functions.php
+++ src/Autoload0/functions.php
@@ -12,11 +12,15 @@ namespace ZoPsr\Autoload0;
 function add_directory(string $directory): void {
 	$directory = rtrim($directory, '/\\');
 
-	spl_autoload_register(function ($class) use ($directory) {
-		$path = strtr($class, ['\\' => '/', '_' => '/']) . '.php';
+	spl_autoload_register(function ($fqcn) use ($directory) {
+		$namespaces = explode('\\', $fqcn);
+		$class = array_pop($namespaces);
+		$class_file = strtr($class, ['_' => DIRECTORY_SEPARATOR]) . '.php';
+		$ns_dir = implode(DIRECTORY_SEPARATOR, $namespaces);
+		$file_path = implode(DIRECTORY_SEPARATOR, array_filter([$directory, $ns_dir, $class_file], 'strlen'));
 
-		if (is_file($directory . DIRECTORY_SEPARATOR . $path)) {
-			require_once $directory . DIRECTORY_SEPARATOR . $path;
+		if (is_file($file_path)) {
+			require_once $file_path;
 		}
 	});
 }
